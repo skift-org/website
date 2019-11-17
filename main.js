ee55023fb4e394a4ec10a66ea567b85e8b2b5776
Cristian Andres Vargas Gonzalez <cvg97@misena.edu.co>
2019-11-17 10:18:12 -0500

    js: Better use of string literals.
    * Update main.js
    
    * links are added to the navigation bar
    
    * Styles were added to the <a> tag, a .readme file was added
    
    * link website
    
    website link added
    
    *  string literals were added
    
    * commented css property removed

--- main.js
+++ main.js
@@ -5,24 +5,25 @@ function update_releases()
     fetch('https://api.github.com/repos/skiftOS/skift/releases')
     .then(res => res.json())
     .then((out) => {
-        console.log(out);
+      
         for (const key in out) 
         {
             let release = out[key];
-            let item_content = "<li>"
+            let item_content = `<li>`;
 
-            item_content += release["tag_name"] + " ";
+            item_content += `${release["tag_name"]}`;
 
             for (const keyAsset in release["assets"]) 
             {
                 let asset = release["assets"][keyAsset];
-                item_content += "<a href=" + asset["browser_download_url"] + ">" + asset["name"] + "</a> " ;
+                item_content += `<a href=${asset["browser_download_url"]}> ${asset["name"]} </a>`;
+                
             }
+           
+            item_content += `<a class="link" href=${release["tarball_url"]}> targball </a>`;
+            item_content += `<a class="link" href=${release["zipball_url"]}> zipball </a>`;
 
-            item_content += "<a href=" + release["tarball_url"] + ">targball</a> " ;
-            item_content += "<a href=" + release["zipball_url"] + ">zipball</a> " ;
-
-            item_content += "</li>"
+            item_content += `</li>`;
 
             releases_list.innerHTML = item_content;
         }
