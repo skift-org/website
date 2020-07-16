15deaf9134d1e946cac8f9ddccac6cc4dbd1a2eb
busybox11 <planetgeek11@gmail.com>
2020-07-17 00:24:00 +0200

    Improved release UI

--- main.js
+++ main.js
@@ -2,27 +2,40 @@ function update_releases()
 {
     let releases_list = document.getElementById("releases-list")
 
+    let latest_release_name = document.getElementById("latest-release-name")
+    let release_list_div = document.getElementById("release-list-div")
+
     fetch('https://api.github.com/repos/skiftOS/skift/releases/latest')
     .then(res => res.json())
     .then((out) => {
 
         let release = out;
-        let item_content = `<li>`;
 
-        item_content += `${release["tag_name"]}`;
+        let release_list_content = ""
+
+        latest_release_name.innerHTML = release["tag_name"];
 
         for (const keyAsset in release["assets"])
         {
+            let icon
             let asset = release["assets"][keyAsset];
-            item_content += `<a class="link-download" href=${asset["browser_download_url"]}>${asset["name"]}</a>`;
+
+            if (asset["name"] == "bootdisk.iso")
+            {
+                icon = "assets/iso-24px.svg"
+            }
+            else
+            {
+                icon = "assets/file-24px.svg"
+            }
+
+            release_list_content += `<a href="${asset["browser_download_url"]}" class="dl-item"><img src="${icon}" class="icon-dl">${asset["name"]}</a>`;
         }
 
-        item_content += `<a class="link-download" href=${release["tarball_url"]}>tarball</a>`;
-        item_content += `<a class="link-download" href=${release["zipball_url"]}>zipball</a>`;
+        release_list_content += `<a href="${release["tarball_url"]}" class="dl-item"><img src="assets/zipped-24px.svg" class="icon-dl">tarball</a>`;
+        release_list_content += `<a href="${release["zipball_url"]}" class="dl-item"><img src="assets/zipped-24px.svg" class="icon-dl">zipball</a>`;
 
-        item_content += `</li>`;
-
-        releases_list.innerHTML += item_content;
+        release_list_div.innerHTML += release_list_content
 
     }).catch(err => releases_list.innerHTML = "Failled to fetch releases... sorry :/");
 }
