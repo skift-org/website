94b670f2b0c66b713f01f01e8fa349bcbeea1ac2
busybox11 <planetgeek11@gmail.com>
2020-07-16 19:56:47 +0200

    Only fetch latest release

--- main.js
+++ main.js
@@ -2,30 +2,28 @@ function update_releases()
 {
     let releases_list = document.getElementById("releases-list")
 
-    fetch('https://api.github.com/repos/skiftOS/skift/releases')
+    fetch('https://api.github.com/repos/skiftOS/skift/releases/latest')
     .then(res => res.json())
     .then((out) => {
 
-        for (const key in out)
+        let release = out;
+        let item_content = `<li>`;
+
+        item_content += `${release["tag_name"]}`;
+
+        for (const keyAsset in release["assets"])
         {
-            let release = out[key];
-            let item_content = `<li>`;
-
-            item_content += `${release["tag_name"]}`;
-
-            for (const keyAsset in release["assets"])
-            {
-                let asset = release["assets"][keyAsset];
-                item_content += `<a class="link-download" href=${asset["browser_download_url"]}>${asset["name"]}</a>`;
-            }
-
-            item_content += `<a class="link-download" href=${release["tarball_url"]}>tarball</a>`;
-            item_content += `<a class="link-download" href=${release["zipball_url"]}>zipball</a>`;
-
-            item_content += `</li>`;
-
-            releases_list.innerHTML += item_content;
+            let asset = release["assets"][keyAsset];
+            item_content += `<a class="link-download" href=${asset["browser_download_url"]}>${asset["name"]}</a>`;
         }
+
+        item_content += `<a class="link-download" href=${release["tarball_url"]}>tarball</a>`;
+        item_content += `<a class="link-download" href=${release["zipball_url"]}>zipball</a>`;
+
+        item_content += `</li>`;
+
+        releases_list.innerHTML += item_content;
+
     }).catch(err => releases_list.innerHTML = "Failled to fetch releases... sorry :/");
 }
 
