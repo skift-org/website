2d77a9ecf9d873929b6b8f4fef15a82d1a4c8fa3
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-12-18 22:58:54 +0100

    Made the site easier to serve from Karm Web.

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function Separator() {
 function NavBar() {
   return <nav className="sticky top-0 px-8 py-6 bg-gray-950 bg-opacity-70 border-b border-white border-opacity-10 backdrop-filter backdrop-blur">
     <div className="flex gap-4 items-center max-w-screen-xl m-auto">
-      <img className="max-h-6" src="logo.svg" alt="" />
+      <img className="max-h-6" src="static/logo.svg" alt="" />
       <div className="flex-grow"></div>
       <Link href="#goal">Goal</Link>
       <Link href="#features">Features</Link>
@@ -56,7 +56,7 @@ function NavBar() {
 
 function Hero() {
   return <TwoColumnsSection>
-    <img class="p-8" src="laptop.png" alt="" />
+    <img class="p-8" src="static/laptop.png" alt="" />
     <div>
       <Title>Skift</Title>
 
@@ -86,7 +86,7 @@ function Goals() {
       <Title>Goal</Title>
 
       <div className="text-gray-400 mb-4">
-        Written by <Link href="https://github.com/sleepy-monax"><img className="inline-block h-6 rounded-full me-1" src="me.jpg" alt="" />sleepy-monax</Link>
+        Written by <Link href="https://github.com/sleepy-monax"><img className="inline-block h-6 rounded-full me-1" src="static/me.jpg" alt="" />sleepy-monax</Link>
       </div>
 
       <p className="mb-4">
@@ -98,7 +98,7 @@ function Goals() {
       </p>
     </div>
 
-    <img className="rounded" src="capture2.png" alt="" />
+    <img className="rounded" src="static/capture2.png" alt="" />
   </TwoColumnsSection>
 }
 
@@ -106,7 +106,7 @@ function Goals() {
 
 function App(props) {
   return <div className="flex flex-col items-center text-center capitalize w-16 gap-2">
-    <img src={"apps/" + props.name + ".png"} alt="" />
+    <img src={"static/apps/" + props.name + ".png"} alt="" />
     {props.name.replace('-', ' ')}
   </div>
 }
@@ -122,7 +122,7 @@ function Beautiful() {
         skiftOS has a beautiful and modern user interface. It's designed to be simple, intuitive, and easy to use.
       </p>
     </div>
-    <img className="rounded" src="capture.png" alt="" />
+    <img className="rounded" src="static/capture.png" alt="" />
   </TwoColumns>
 }
 
@@ -167,7 +167,7 @@ function RejectPosix() {
       </p>
     </div>
     <div className="flex flex-col items-center gap-4">
-      <img className="h-56" src="nolinux.png" alt="" />
+      <img className="h-56" src="static/nolinux.png" alt="" />
     </div>
   </TwoColumns>
 }
@@ -200,31 +200,31 @@ function Technologies() {
     <Title>Technologies</Title>
     <div className="flex flex-col md:grid md:grid-cols-3 items-top gap-8 pt-4">
       <Tech
-        icon="techs/tech-karm.png"
+        icon="static/techs/tech-karm.png"
         title="Modern Core Framework"
         desc="A modern C++ core framework that make C++ delightful to use and provides a solid foundation for building applications and libraries."
         link="https://github.com/skift-org/skift/tree/main/src/libs" />
 
       <Tech
-        icon="techs/tech-hjert.png"
+        icon="static/techs/tech-hjert.png"
         title="Capability-based Microkernel"
         desc="A capability-based microkernel that provides security and modularity."
         link="https://github.com/skift-org/skift/tree/main/src/kernel" />
 
       <Tech
-        icon="techs/tech-opstart.png"
+        icon="static/techs/tech-opstart.png"
         title="UEFI Bootloader"
         desc="An UEFI bootloader that support a wide range of customizations with a beautiful graphical interface."
         link="https://github.com/skift-org/skift/tree/main/src/kernel/loader" />
 
       <Tech
-        icon="techs/tech-hideo.png"
+        icon="static/techs/tech-hideo.png"
         title="Graphical Shell"
         desc="A responsive and beautiful graphical shell, supporting desktop and mobile devices."
         link="https://github.com/skift-org/skift/tree/main/src/apps/shell-app" />
 
       <Tech
-        icon="techs/tech-cutekit.png"
+        icon="static/techs/tech-cutekit.png"
         title="Multitarget Build System"
         desc="A multitarget build system that support building for ARM, x86, and RISC-V."
         link="https://github.com/cute-engineering/cutekit" />
@@ -247,10 +247,10 @@ function Social() {
         Join the Community!
       </Title>
       <div className="grid grid-cols-2 md:flex md:flex-row gap-8 mt-8">
-        <SocialItem image="github.svg" name="GitHub" href="https://github.com/skift-org" />
-        <SocialItem image="discord.svg" name="Discord" href="http://discord.skiftos.org/" />
-        <SocialItem image="reddit.svg" name="Reddit" href="https://www.reddit.com/r/skift" />
-        <SocialItem image="twitter.svg" name="Twitter" href="https://twitter.com/search?q=%23skiftOS" />
+        <SocialItem image="static/github.svg" name="GitHub" href="https://github.com/skift-org" />
+        <SocialItem image="static/discord.svg" name="Discord" href="http://discord.skiftos.org/" />
+        <SocialItem image="static/reddit.svg" name="Reddit" href="https://www.reddit.com/r/skift" />
+        <SocialItem image="static/twitter.svg" name="Twitter" href="https://twitter.com/search?q=%23skiftOS" />
       </div>
     </div>
   </Section>
@@ -258,18 +258,18 @@ function Social() {
 
 function Footer() {
   return <div className="text-center p-8 flex flex-col items-center gap-4">
-    <img className="w-10" src="skift.svg" alt="" />
+    <img className="w-10" src="static/skift.svg" alt="" />
     "The delightful operating system"<br />
     Copyright © 2018-2023 Cute Engineering<br />
     All rights reserved.<br />
     <a href="https://github.com/skift-org/website"> Source code</a>
-    <img className="w-10" src="glider.svg" alt="" />
+    <img className="w-10" src="static/glider.svg" alt="" />
   </div>
 }
 
 function Page() {
   return (
-    <div className="bg-cover bg-center bg-fixed" style={{ backgroundImage: "url(wallpaper.png)" }}>
+    <div className="bg-cover bg-center bg-fixed" style={{ backgroundImage: "url(static/wallpaper.png)" }}>
       <NavBar />
       <Hero />
       <div class="bg-gray-950">
