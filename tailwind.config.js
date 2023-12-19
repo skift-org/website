e9f80708ebdb90183e4c943aad548f32ba3994da
VAN BOSSUYT Nicolas <nivb@odoo.com>
2023-12-19 10:07:47 +0100

    Migrated to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
 const colors = require('tailwindcss/colors')
 
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/index.html"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
       transparent: 'transparent',
@@ -28,3 +30,4 @@ module.exports = {
   },
   plugins: [],
 }
+
